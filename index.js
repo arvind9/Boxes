bd039cab64ae1dbb52dc73f1b638efa083f51af3
Narender Narra <arvind1359@gmail.com>
2025-05-14 15:49:33 +0000

    2nd commit

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const Box = props => {
   //  Write your code here.
+  const {boxText, className} = props
+  return (
+    <div className={`box ${className}`}>
+      <p>{boxText}</p>
+    </div>
+  )
 }
 
 const element = (
-  //  Write your code here. 
+  //  Write your code here.
+  <div className='bg-container'>
+    <h1>Boxes</h1>
+    <div className='box-container'>
+      <Box className='box-small' boxText='Small' />
+      <Box className='box-medium' boxText='Medium' />
+      <Box className='box-large' boxText='Large' />
+    </div>
+  </div>
 )
 
-ReactDOM.render(element, document.getElementById("root"));
+ReactDOM.render(element, document.getElementById('root'))
